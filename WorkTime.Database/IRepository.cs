29593bd7f1f6a97f6a4d063b2f78ceb36278faeb
Tamás Simon <tsimon@geomant.com>
2020-11-07 13:23:12 +0100

    Added DAL project

---
+++ WorkTime.Database/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace WorkTime.Database
+{
+	public interface IRepository<T>
+	{
+		Task Create(T obj);
+		Task<T> Get(int id);
+		Task<T> Update(T obj);
+		Task Delete(int id);
+	}
+}
---
+++ WorkTime.Database/WorkTimeDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkTime.Database
+{
+	public class WorkTimeDbContext:DbContext
+	{
+	}
+}
