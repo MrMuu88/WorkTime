156232e71f101f2ecdcff0aa09bbfbb58257d916
Tamás Simon <tsimon@geomant.com>
2020-11-10 06:57:06 +0100

    Moved Unsubscribe to IMessenger

--- WorkTime/Interfaces/IMessenger.cs
+++ WorkTime/Interfaces/IMessenger.cs
@@ -6,5 +6,6 @@ namespace WorkTime.Interfaces
 	{
 		void Publish<T>(T message) where T : class;
 		void Subscribe<T>(Action<dynamic> action) where T : class;
+		void UnSubscribe<TMessage>(object subscriber) where TMessage : class;
 	}
 }
