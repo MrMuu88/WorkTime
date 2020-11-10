1604dd51d0378f902c78dec27ef8c64dbcbc90cd
Tamás Simon <tsimon@geomant.com>
2020-11-10 05:46:59 +0100

    Implemented Messenger component

---
+++ WorkTime/Components/MessengerComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WorkTime.Interfaces;
+
+namespace WorkTime.Components
+{
+	public class MessengerComponent:IMessenger
+	{
+		public Dictionary<Type,List<Action<object>>> SubscriberStore { get; private set; } = new Dictionary<Type,List<Action<object>>>();
+
+		public void Subscribe<TMessage>(Action<dynamic> action) where TMessage:class
+		{
+			if (SubscriberStore.ContainsKey(typeof(TMessage)))
+				SubscriberStore[typeof(TMessage)].Add(action);
+			else
+				SubscriberStore.Add(typeof(TMessage),new List<Action<object>>() {action});
+		}
+
+		public void Publish<TMessage>(TMessage message) where TMessage : class
+		{
+			foreach (var action in SubscriberStore[typeof(TMessage)]) {
+					action(message);
+			}
+		}
+
+		public void UnSubscribe<TMessage>(object subscriber) where TMessage : class 
+		{
+			var shouldRemove = SubscriberStore[typeof(TMessage)].Where(action => action.Target == subscriber).ToList();
+			shouldRemove.ForEach(removeItem => SubscriberStore[typeof(TMessage)].Remove(removeItem));
+		}
+	}
+}
---
+++ WorkTime/Interfaces/IMessenger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WorkTime.Interfaces
+{
+	public interface IMessenger
+	{
+		void Publish<T>(T message) where T : class;
+		void Subscribe<T>(Action<dynamic> action) where T : class;
+	}
+}
