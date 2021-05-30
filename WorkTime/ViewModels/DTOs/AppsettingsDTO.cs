afb94495d7e9cc76c785caf561fd5417d657f74c
Tamás Simon <tsimon@geomant.com>
2021-05-30 11:16:43 +0200

    Modified AppsettingsDTO to hold the required vaues

--- WorkTime/ViewModels/DTOs/AppsettingsDTO.cs
+++ WorkTime/ViewModels/DTOs/AppsettingsDTO.cs
@@ -1,7 +1,32 @@
-﻿namespace WorkTime.ViewModels.DTOs
+﻿using WorkTime.Core.Models;
+
+namespace WorkTime.ViewModels.DTOs
 {
 	public class AppsettingsDTO : ViewModelBase
 	{
+		private string pesonalAccessToken;
+		private WorkItemType startMenuAt;
+		private bool myAsigmentsOnly;
+		private bool startWithWindows;
 
+		public string PesonalAccessToken {
+			get => pesonalAccessToken;
+			set { pesonalAccessToken = value; Changed(); }
+		}
+
+		public WorkItemType StartMenuAt {
+			get => startMenuAt;
+			set { startMenuAt = value; Changed(); } 
+		}
+
+		public bool MyAsigmentsOnly {
+			get => myAsigmentsOnly;
+			set { myAsigmentsOnly = value; Changed(); }
+		}
+
+		public bool StartWithWindows {
+			get => startWithWindows;
+			set { startWithWindows = value; Changed(); }
+		}
 	}
 }
